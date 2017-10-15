2196e54ec37ec6d71a7aa1511ecb95939b11aa1a
Carlo Lam <calam12@gmail.com>
2017-10-15 01:07:49 -0700

    return name of the object in the model

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,4 +5,10 @@ class RestaurantLocation(models.Model):
     name       =  models.CharField(max_length=120)
     location   =  models.CharField(max_length=120,null=True, blank=True)
     category   =  models.CharField(max_length=120,null=True, blank=False)
+    timestamp  =  models.DateTimeField(auto_now=True)
+    updated    =  models.DateTimeField(auto_now=True)
+    #my_date_field = models.DateField(auto_now=False, auto_now_add=False)
+    def __str__(self):
+        return self.name
+
 
--- restaurants/views.py
+++ restaurants/views.py
@@ -3,21 +3,18 @@ from django.http import HttpResponse
 
 from django.views import View
 from django.views.generic import TemplateView
+from .models import RestaurantLocation
+
 # Create your views here.
-import random
+def restaurant_listview(request):
+    template_name = "restaurants/restaurants_list.html"
+    queryset = RestaurantLocation.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request,template_name, context)
 
 
-class HomeView(TemplateView):
-    template_name="home.html"
-    def get_context_data(self, *args,**kwargs):
-        num = random.randint(0,100000)
-        some_list = [num, random.randint(0,100000),random.randint(0,100000)]
-        context = {
-            "bool_item":False,
-            "num": num,
-            "some_list":some_list
-        }
-        print(context)
-        return context
+
 
 
