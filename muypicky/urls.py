3decd799871ad614e3acfad535ee0bcdd164d26a
Carlo Lam <calam12@gmail.com>
2017-10-17 23:56:11 -0700

    additition of search restaurant view with slug

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from restaurants.views import restaurant_listview
+from restaurants.views import restaurant_listview, RestaurantListView,  SearchRestaurantListView
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^about/',TemplateView.as_view(template_name="about.html")),
-    url(r'^restaurants/$',restaurant_listview),
+    url(r'^restaurants/$',RestaurantListView.as_view()),
+    url(r'^restaurants/(?P<slug>\w+)/$', SearchRestaurantListView.as_view()),
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html")),
     url(r'^$', TemplateView.as_view(template_name="home.html")),
     
--- restaurants/views.py
+++ restaurants/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from .models import RestaurantLocation
 
 # Create your views here.
@@ -14,7 +14,21 @@ def restaurant_listview(request):
     }
     return render(request,template_name, context)
 
+class RestaurantListView(ListView):
+    queryset = RestaurantLocation.objects.all()
+    template_name = "restaurants/restaurants_list.html"
 
-
-
+class SearchRestaurantListView(ListView):
+    template_name = "restaurants/restaurants_list.html"
+    def get_queryset(self):
+        print(self.kwargs)
+        slug = self.kwargs
+        if slug:
+            queryset = RestaurantLocation.objects.filter(category__iexact='Mexican' )
+        else:
+            queryset = RestaurantListView.objects.none()
+        return queryset
+class AsianRestaurantListView(ListView):
+    queryset = RestaurantLocation.objects.filter(category__iexact='Asian Fusion' )
+    template_name = "restaurants/restaurants_list.html"
 
