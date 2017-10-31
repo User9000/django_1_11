abcbc81233a08249febbb8044fceba813cc09a4f
Carlo Lam <calam12@gmail.com>
2017-10-30 22:23:12 -0700

    added new function in models, signal handler  to save a slug to the url

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -15,18 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
-from restaurants.views import restaurant_listview, RestaurantListView
+from restaurants.views import restaurant_listview, RestaurantListView, RestaurantDetailView
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^about/',TemplateView.as_view(template_name="about.html")),
     url(r'^restaurants/$', RestaurantListView.as_view()),
-    url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
+    #url(r'^restaurants/(?P<slug>\w+)/$', RestaurantListView.as_view()),
+    url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html")),
     url(r'^$', TemplateView.as_view(template_name="home.html")),
     
-    ]
+ ]
 
 
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,11 +1,11 @@
 
 
 from django.db.models import Q
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
+from django.http import HttpResponse
 from django.views import View
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView,DetailView
 from .models import RestaurantLocation
 
 # Create your views here.
@@ -19,7 +19,7 @@ def restaurant_listview(request):
 
 
 class RestaurantListView(ListView):
-    template_name = "restaurants/restaurants_list.html"
+   
     def get_queryset(self):
         slug = self.kwargs.get('slug')
         if slug:
@@ -30,3 +30,13 @@ class RestaurantListView(ListView):
             print("we did not")
 
         return queryset
+
+
+class RestaurantDetailView(DetailView):
+    
+    queryset = RestaurantLocation.objects.all()    
+"""    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get("rest_id")
+        obj = get_object_or_404(RestaurantLocation,id=rest_id)
+        return obj
+"""
