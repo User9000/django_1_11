2946ca3ea426191d022a745a9f110b2ea864d31f
Carlo Lam <calam12@gmail.com>
2017-11-05 19:16:49 -0800

    addition of validators.py

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.db.models.signals import pre_save 
-
 from .utils import unique_slug_generator
+from  .validators import validate_category
 
 # Create your models here.
 class RestaurantLocation(models.Model):
     name       =  models.CharField(max_length=120)
     location   =  models.CharField(max_length=120,null=True, blank=True)
-    category   =  models.CharField(max_length=120,null=True, blank=False)
+    category   =  models.CharField(max_length=120,null=True, blank=False, validators =[validate_category])
     timestamp  =  models.DateTimeField(auto_now=True)
     updated    =  models.DateTimeField(auto_now=True)
     slug       =  models.SlugField(null=True, blank=True)
@@ -24,6 +24,7 @@ class RestaurantLocation(models.Model):
 
 ### Signals handlers
 def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    instance.category = instance.category.capitalize()
     print('saving....')
     print(instance.timestamp)
     if not instance.slug:
