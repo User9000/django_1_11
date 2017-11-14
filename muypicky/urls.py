8995d8ba08a9160ec5f35668aee2339c22a142a2
Carlo Lam <calam12@gmail.com>
2017-11-13 22:15:31 -0800

    added reverse and fixed url

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from restaurants.views import restaurant_listview, ResturantCreateView,  RestaurantListView, RestaurantDetailView, restaurant_createview
 from django.views.generic import TemplateView
 
 from django.contrib.auth.views import LoginView, PasswordResetView
+
+
 
 urlpatterns = [
 
@@ -27,12 +29,9 @@ urlpatterns = [
     url(r'^login/', LoginView.as_view(), name = 'login'),
     url(r'^login/', PasswordResetView.as_view(),name='password_reset'),
     url(r'^about/',TemplateView.as_view(template_name="about.html"),name='about'),
-    url(r'^restaurants/$', RestaurantListView.as_view(),name='restaurants'),
-    url(r'^restaurants/create/$',ResturantCreateView.as_view(),name='restaurants-create'),
-    #url(r'^restaurants/create/$',restaurant_createview),
-    url(r'^restaurants/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view(), name='restaurant-detail'),
+    url(r'^restaurants/', include('restaurants.urls', namespace='restaurants')),
     url(r'^contact/$', TemplateView.as_view(template_name="contact.html"),name='contact'),
-    #url(r'^$', TemplateView.as_view(template_name="home.html")),
+
     
  ]
 
--- restaurants/models.py
+++ restaurants/models.py
@@ -2,8 +2,10 @@
 from django.conf import settings
 from django.db import models
 from django.db.models.signals import pre_save 
+
 from .utils import unique_slug_generator
 from  .validators import validate_category
+
 from django.core.urlresolvers import reverse
 
 User = settings.AUTH_USER_MODEL
@@ -22,8 +24,10 @@ class RestaurantLocation(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('restaurant-detail', kwargs={'slug': self.slug } )
+    def get_absolute_url(self): #get_absolute_url
+        #return f"/restaurants/{self.slug}" 
+        return reverse('restaurants:detail', kwargs={'slug':self.slug})
+
 
     @property
     def title(self):
